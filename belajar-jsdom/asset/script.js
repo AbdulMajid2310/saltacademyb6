c5410f70889aad770d85bf7992e013b8070953d4
AbdulMajid2310 <Abdulma272@gmail.com>
2023-04-02 20:30:44 +0700

    menambah file

--- belajar-jsdom/asset/script.js
+++ belajar-jsdom/asset/script.js
@@ -1,3 +1,30 @@
-const addCarForm = document.getElementById { "addCarform" };
+const addCarForm = document.getElementById('addCarForm');
+const cardContainer = document.getElementById('card_container');
 
-addCarForm.addEventListener("submit", e)
+addCarForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log("Submit Form")
+
+    const ownerElement = document.getElementById('owner');
+    const platnoElement = document.getElementById('platnum');
+    const brandElement = document.getElementById('brand');
+
+    const ownerValue = ownerElement.value;
+    const platnoValue = platnoElement.value;
+    const brandValue = brandElement.value;
+
+    addElement(ownerValue, platnoValue, brandValue);
+
+    //reset form
+    ownerElement.value = ''
+    platnoElement.value = ''
+
+
+})
+
+function addElement(ownerValue, platnoValue, brandValue) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = '<p>' + ownerValue + '</p>'
+
+    cardContainer.appendChild(newElement);
+}
